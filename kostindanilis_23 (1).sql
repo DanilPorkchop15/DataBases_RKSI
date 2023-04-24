20f2058dc22d806c744a793eea1b03b16be9bc72
Danil Kostin <114452500+DanilPorkchop15@users.noreply.github.com>
2023-04-24 14:20:03 +0300

    Add files via upload

---
+++ kostindanilis_23 (1).sql
@@ -0,0 +1,377 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 20 2023 г., 18:13
+-- Версия сервера: 8.0.19
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `kostindanilis_23`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `customers`
+--
+
+CREATE TABLE `customers` (
+  `cnum` int NOT NULL,
+  `cname` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pol` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `city` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  `snum` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `customers`
+--
+
+INSERT INTO `customers` (`cnum`, `cname`, `pol`, `city`, `rating`, `snum`) VALUES
+(2001, 'Hoffman', 'м', 'Paris', 100, 1001),
+(2002, 'Giovanni', 'м', 'Rome', 200, 1003),
+(2003, 'Liu', 'ж', 'San Jose', 200, 1002),
+(2004, 'Grass', 'м', 'Berlin', 300, 1002),
+(2006, 'Clemens', 'ж', 'Paris', 100, 1001),
+(2007, 'Pereira', 'м', 'Rome', 100, 1004),
+(2008, 'Cisneros', 'м', 'San Jose', 300, 1007);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `onum` int NOT NULL,
+  `amt` float(10,2) DEFAULT NULL,
+  `dostavka` int DEFAULT NULL,
+  `oplata` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `odate` date DEFAULT NULL,
+  `cnum` int DEFAULT NULL,
+  `snum` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`onum`, `amt`, `dostavka`, `oplata`, `odate`, `cnum`, `snum`) VALUES
+(3001, 18.69, 1000, ' наличный', '2021-03-10', 2008, 1007),
+(3002, 1900.10, 2000, 'безналичный', '2021-03-10', 2007, 1004),
+(3003, 767.19, 2316, ' наличный', '2021-03-10', 2001, 1001),
+(3005, 5160.45, 1245, 'безналичный', '2021-03-10', 2003, 1003),
+(3006, 1098.16, 7654, ' наличный', '2021-03-10', 2008, 1007),
+(3007, 75.75, 4324, ' наличный', '2021-04-10', 2004, 1002),
+(3008, 4723.00, 7644, 'безналичный', '2021-05-10', 2006, 1001),
+(3009, 1713.23, 5392, 'безналичный', '2021-04-10', 2002, 1003),
+(3010, 1309.95, 1240, ' наличный', '2021-06-10', 2004, 1002);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `salespeople`
+--
+
+CREATE TABLE `salespeople` (
+  `snum` int NOT NULL,
+  `sname` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `naprav` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `city` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `comm` float(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `salespeople`
+--
+
+INSERT INTO `salespeople` (`snum`, `sname`, `naprav`, `city`, `comm`) VALUES
+(1001, 'Peel', 'табачные изделия', 'London', 0.12),
+(1002, 'Serres', 'молочная продукция', 'San Jose', 0.16),
+(1003, 'Axelrod', 'колбасная продукция', 'New York', 0.10),
+(1004, 'Motika', 'мясная продукция', 'London', 0.11),
+(1007, 'Rifkin', 'алкогольная продукция', 'Barcelona', 0.16);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v71`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v71` (
+`city` varchar(30)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v72`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v72` (
+`rating` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v73`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v73` (
+`comm` float(3,2)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v74`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v74` (
+`city` varchar(30)
+,`comm` float(3,2)
+,`naprav` varchar(30)
+,`sname` varchar(30)
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v75`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v75` (
+`city` varchar(30)
+,`comm` float(3,2)
+,`naprav` varchar(30)
+,`sname` varchar(30)
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v76`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v76` (
+`city` varchar(30)
+,`cname` varchar(30)
+,`cnum` int
+,`pol` varchar(30)
+,`rating` int
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v77`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v77` (
+`city` varchar(30)
+,`cname` varchar(30)
+,`cnum` int
+,`pol` varchar(30)
+,`rating` int
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v78`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v78` (
+`amt` float(10,2)
+,`cnum` int
+,`dostavka` int
+,`odate` date
+,`onum` int
+,`oplata` varchar(30)
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v79`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v79` (
+`city` varchar(30)
+,`cname` varchar(30)
+,`cnum` int
+,`pol` varchar(30)
+,`rating` int
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Дублирующая структура для представления `v710`
+-- (См. Ниже фактическое представление)
+--
+CREATE TABLE `v710` (
+`city` varchar(30)
+,`cname` varchar(30)
+,`cnum` int
+,`pol` varchar(30)
+,`rating` int
+,`snum` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v71`
+--
+DROP TABLE IF EXISTS `v71`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v71`  AS SELECT DISTINCT `customers`.`city` AS `city` FROM `customers` ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v72`
+--
+DROP TABLE IF EXISTS `v72`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v72`  AS SELECT DISTINCT `customers`.`rating` AS `rating` FROM `customers` ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v73`
+--
+DROP TABLE IF EXISTS `v73`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v73`  AS SELECT DISTINCT `salespeople`.`comm` AS `comm` FROM `salespeople` ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v74`
+--
+DROP TABLE IF EXISTS `v74`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v74`  AS SELECT `salespeople`.`snum` AS `snum`, `salespeople`.`sname` AS `sname`, `salespeople`.`naprav` AS `naprav`, `salespeople`.`city` AS `city`, `salespeople`.`comm` AS `comm` FROM `salespeople` WHERE (`salespeople`.`city` = 'London') ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v75`
+--
+DROP TABLE IF EXISTS `v75`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v75`  AS SELECT `salespeople`.`snum` AS `snum`, `salespeople`.`sname` AS `sname`, `salespeople`.`naprav` AS `naprav`, `salespeople`.`city` AS `city`, `salespeople`.`comm` AS `comm` FROM `salespeople` WHERE (`salespeople`.`comm` > 0.12) ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v76`
+--
+DROP TABLE IF EXISTS `v76`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v76`  AS SELECT `customers`.`cnum` AS `cnum`, `customers`.`cname` AS `cname`, `customers`.`pol` AS `pol`, `customers`.`city` AS `city`, `customers`.`rating` AS `rating`, `customers`.`snum` AS `snum` FROM `customers` WHERE (`customers`.`city` = 'Rome') ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v77`
+--
+DROP TABLE IF EXISTS `v77`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v77`  AS SELECT `customers`.`cnum` AS `cnum`, `customers`.`cname` AS `cname`, `customers`.`pol` AS `pol`, `customers`.`city` AS `city`, `customers`.`rating` AS `rating`, `customers`.`snum` AS `snum` FROM `customers` WHERE (`customers`.`rating` in (100,300)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v78`
+--
+DROP TABLE IF EXISTS `v78`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v78`  AS SELECT `orders`.`onum` AS `onum`, `orders`.`amt` AS `amt`, `orders`.`dostavka` AS `dostavka`, `orders`.`oplata` AS `oplata`, `orders`.`odate` AS `odate`, `orders`.`cnum` AS `cnum`, `orders`.`snum` AS `snum` FROM `orders` WHERE (`orders`.`amt` < 1000) ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v79`
+--
+DROP TABLE IF EXISTS `v79`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v79`  AS SELECT `customers`.`cnum` AS `cnum`, `customers`.`cname` AS `cname`, `customers`.`pol` AS `pol`, `customers`.`city` AS `city`, `customers`.`rating` AS `rating`, `customers`.`snum` AS `snum` FROM `customers` WHERE ((`customers`.`rating` = 200) AND (`customers`.`city` = 'Rome')) ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура для представления `v710`
+--
+DROP TABLE IF EXISTS `v710`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`127.0.0.1` SQL SECURITY DEFINER VIEW `v710`  AS SELECT `customers`.`cnum` AS `cnum`, `customers`.`cname` AS `cname`, `customers`.`pol` AS `pol`, `customers`.`city` AS `city`, `customers`.`rating` AS `rating`, `customers`.`snum` AS `snum` FROM `customers` WHERE ((`customers`.`rating` = 300) OR (`customers`.`city` = 'Berlin')) ;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`cnum`),
+  ADD KEY `snum` (`snum`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`onum`),
+  ADD KEY `snum` (`snum`),
+  ADD KEY `cnum` (`cnum`);
+
+--
+-- Индексы таблицы `salespeople`
+--
+ALTER TABLE `salespeople`
+  ADD PRIMARY KEY (`snum`);
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`snum`) REFERENCES `salespeople` (`snum`);
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`snum`) REFERENCES `salespeople` (`snum`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`cnum`) REFERENCES `customers` (`cnum`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
